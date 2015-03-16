50cfb5d6bca5e9c34905d5c539673ad33352244f
Marcel Sackermann <marcel@iras-revier.de>
2015-03-16 12:43:19 +0100

    rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_testapp_session'
+Rails.application.config.session_store :cookie_store, key: '_dart_websocket_rails_sample_session'
